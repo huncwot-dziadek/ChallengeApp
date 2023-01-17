07524f4911149b43382b560d916f7715645b6b4f
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2023-01-17 20:49:20 +0100

    Zadanie Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string name = "Krzysztof";
+char sex = 'M';
+int age = 55;
+
+if((sex == 'K') && (age < 30)) 
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+if((name == "Ewa") && (age == 33))
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+if ((sex == 'M') && (age < 18))
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
