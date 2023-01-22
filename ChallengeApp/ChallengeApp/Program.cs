a4646c0b6fbb5ca2dbb526ec7d27d7c470a519a0
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2023-01-22 12:13:31 +0100

    Day 6 class User

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,4 +1,6 @@
-﻿User user1 = new User("Adam", "123456AAA");
+﻿using ChallengeApp;
+
+User user1 = new User("Adam", "123456AAA");
 User user2 = new User("Ewa", "123456AAA");
 User user3 = new User("Karol", "123456AAA");
 User user4 = new User("Mietek", "123456AAA");
@@ -8,34 +10,4 @@ user1.AddScore(1);
 user1.AddScore(9);
 var result = user1.Result;
 Console.WriteLine(result);
-
-
-class User
-{
-    private List<int> score = new List<int>();
-
-    public User(string login, string password)
-    {
-        this.Login = login;
-        this.Password = password;
-
-    }
-    public string Login { get; private set; }
-    public string Password { get; private set; }
-
-    public int Result
-    {
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-
-    public void AddScore(int number)
-    {
-        this.score.Add(number);
-    }
-}
-
-
 
--- ChallengeApp/ChallengeApp/User.cs
+++ ChallengeApp/ChallengeApp/User.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
-    internal class User
+    public class User
     {
+        private List<int> score = new List<int>();
+
+        public User(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+
+        }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
     }
 }
