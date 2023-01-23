e82f4da10493798e77762f56bb591f953b0c878a
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2023-01-23 15:20:50 +0100

    Emploee

---
+++ ChallengeApp/ChallengeApp/Emploee.cs
@@ -0,0 +1,31 @@
+﻿namespace ChallengeApp
+{
+    public class Emploee
+    {
+        private List<int> score = new List<int>();
+        public Emploee(string name, string surname, string years)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Years = years;
+        }
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public string Years { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+
+    }
+}
