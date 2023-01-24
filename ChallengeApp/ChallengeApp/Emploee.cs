83676d3c46f1b57cf34dac751533cce1f35baff5
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2023-01-24 16:46:44 +0100

    24 zadanie 666

--- ChallengeApp/ChallengeApp/Emploee.cs
+++ ChallengeApp/ChallengeApp/Emploee.cs
@@ -3,12 +3,16 @@
     public class Emploee
     {
         private List<int> score = new List<int>();
+
         public Emploee(string name, string surname, string years)
         {
             this.Name = name;
             this.Surname = surname;
             this.Years = years;
         }
+
+
+
         public string Name { get; private set; }
 
         public string Surname { get; private set; }
@@ -28,4 +32,5 @@
         }
 
     }
+    
 }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,64 @@
 ﻿using ChallengeApp;
+using System.Collections.Immutable;
 
-Emploee emploee1 = new Emploee("Maciej", "Kramarczyk", "29");
+Emploee emploee1 = new Emploee("Maciej", "Kramarczyk", "24");
 Emploee emploee2 = new Emploee("Anna", "Nienowacka", "33");
-Emploee emploee3 = new Emploee("Waldemar", "Pasikonik", "37");
+Emploee emploee3 = new Emploee("Waldemar", "Pasikonik", "40");
 
-emploee1.AddScore(7); emploee1.AddScore(1); emploee1.AddScore(8); emploee1.AddScore(6); emploee1.AddScore(7);
+emploee1.AddScore(7); emploee1.AddScore(1); emploee1.AddScore(8); emploee1.AddScore(6); emploee1.AddScore(9);
 emploee2.AddScore(4); emploee2.AddScore(1); emploee2.AddScore(8); emploee2.AddScore(9); emploee2.AddScore(9);
-emploee3.AddScore(9); emploee3.AddScore(9); emploee3.AddScore(7); emploee3.AddScore(8); emploee3.AddScore(4);
+emploee3.AddScore(9); emploee3.AddScore(9); emploee3.AddScore(9); emploee3.AddScore(3); emploee3.AddScore(1);
 
-var result1 = emploee1.Result;
-var result2 = emploee2.Result;
-var result3 = emploee3.Result;
+List<int> TemporaryList = new List<int>();
+
+TemporaryList.Add(emploee1.Result);
+TemporaryList.Add(emploee2.Result);
+TemporaryList.Add(emploee3.Result);
+
+var max = TemporaryList.Max(); var indexMax = TemporaryList.IndexOf(max);
+var min = TemporaryList.Min(); var indexMin = TemporaryList.IndexOf(min);
+
+string outEmpl1 = "emploee1.name +emploee1.surname +lat emploee1.years ";
+string outEmpl2 = "emploee2.name +emploee2.surname +lat emploee2.years ";
+string outEmpl3 = "emploee3.name +emploee3.surname +lat emploee3.years ";
+
+int k = 6;
+    
+    if (emploee1.Result == emploee2.Result && emploee1.Result == emploee3.Result)
+{
+    Console.WriteLine($"Wszyscy pracownicy osiągnęli ten sam wynik, który wynosi {max}. Są to");
+    Console.WriteLine($"{outEmpl1}");
+    Console.WriteLine("outEmpl2");
+    Console.WriteLine("outEmpl3");
 
 
-Console.WriteLine($"{emploee1.Name} {result1}    {emploee2.Name} {result2}     {emploee3.Name} {result3}");
+
+}
+
+else if (emploee1.Result != emploee2.Result && emploee1.Result != emploee3.Result && emploee2.Result != emploee3.Result)
+{
+    //indexMax
+}
 
 
 
 
+    Console.WriteLine(max);
+Console.WriteLine(indexMax);
+Console.WriteLine(min);
+Console.WriteLine(indexMin);
+
+
+
+
+
+//for (int i = 0; i <= WorkList1.Count; i++)
+
+
+
+
+
+foreach (var ddd in TemporaryList)
+{
+    Console.WriteLine(ddd);
+}
