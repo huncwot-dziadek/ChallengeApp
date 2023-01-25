f92b4d5f609f16a783bd181bc1d0bc8721a24654
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2023-01-25 13:00:13 +0100

    praca godz 13

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -11,108 +11,63 @@ emploee1.AddScore(8);
 emploee1.AddScore(6);
 emploee1.AddScore(9);
 
-emploee2.AddScore(4);
-emploee2.AddScore(1);
-emploee2.AddScore(8);
+emploee2.AddScore(3);
+emploee2.AddScore(22);
+emploee2.AddScore(5);
 emploee2.AddScore(9);
-emploee2.AddScore(10);
+emploee2.AddScore(9);
 
 emploee3.AddScore(9);
-emploee3.AddScore(9);
-emploee3.AddScore(9);
+emploee3.AddScore(39);
+emploee3.AddScore(8);
 emploee3.AddScore(3);
-emploee3.AddScore(3);
+emploee3.AddScore(1);
 
+Console.WriteLine(emploee1.Result);
+Console.WriteLine(emploee2.Result);
+Console.WriteLine(emploee3.Result);
 
 List<Emploee> emploeeTable = new List<Emploee>()
 {
     emploee1, emploee2, emploee3
 };
 
-//List<Emploee> emploeeResults = new List<Emploee>()
-//{
-//    emploee1.Result, emploee2.Result, emploee3.Result
-//};
+List<Emploee> nextWinnerTable = new List<Emploee>() { emploee1};
 
-int[] tableResults = new int[3];
-for (int i = 0; i < 3; i++)
-{
-    tableResults[i] = 0;
-}
-int j = 0;
+int maxResult = -1;
+Emploee emploeeWinner = null;
+Emploee nextWinner = null;
 
 foreach (var emploee in emploeeTable)
 {
-    tableResults[j] = tableResults[j] + emploee.Result;
-    emploee.Result = 0;
+    if (emploee.Result > maxResult)
+    {
+        emploeeWinner = emploee;
+        maxResult = emploee.Result;
+    }
 
-    Console.WriteLine(tableResults[j]);
+    else if (emploee.Result == maxResult)
+    {
+        nextWinner = emploeeWinner;            
+        emploeeWinner = emploee;
 
-
-    //Console.WriteLine(tableResults[i]);
+    }
 }
 
-//int maxResult = -1;
-//Emploee emploeeWithMaxValue = null;
+//Console.WriteLine($"Największą liczbę punktów, tj. {maxResult} uzyskał pracownik:");
+//Console.WriteLine(emploeeWinner.Name+" " + emploeeWinner.Surname + " lat " + emploeeWinner.Years);
+
+
+Console.WriteLine($"Lista pracowników, którzy uzyskali największą liczbę punktów, tj. {maxResult}");
+Console.WriteLine("jest to następująca:");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(emploeeWinner.Name + " " + emploeeWinner.Surname + " lat " + emploeeWinner.Years);
+//Console.WriteLine(nextWinner.Name + " " + nextWinner.Surname + " lat " + nextWinner.Years);
 
 
 
-
-//Console.WriteLine(emploeeWithMaxValue);
-
-//List<int> TemporaryList = new List<int>();
-
-//TemporaryList.Add(emploee1.Result);
-//TemporaryList.Add(emploee2.Result);
-//TemporaryList.Add(emploee3.Result);
-
-//var max = TemporaryList.Max(); 
-//var indexMax = TemporaryList.IndexOf(max);
-//var min = TemporaryList.Min();
-//var indexMin = TemporaryList.IndexOf(min);
-
-//if (emploee1.Result == emploee2.Result && emploee1.Result == emploee3.Result)
+//foreach (var emploee in winnersTable)
 //{
-//    Console.WriteLine($"Wszyscy pracownicy osiągnęli ten sam wynik, który wynosi {max}. Są to:");
-//    Console.WriteLine(emploee1.Name + "  " + emploee1.Surname + " lat " + emploee1.Years);
-//    Console.WriteLine(emploee2.Name + "  " + emploee2.Surname + " lat " + emploee2.Years);
-//    Console.WriteLine(emploee3.Name + "  " + emploee3.Surname + " lat " + emploee3.Years);
-//    Console.WriteLine();
-//}
-
-
-////else if (emploee1.Result != emploee2.Result && emploee1.Result != emploee3.Result && emploee2.Result != emploee3.Result)
-////{
-////    Console.WriteLine("Pracownikiem, który uzyskał najwięcej punktów jest:");
-////    //Console.WriteLine(EmploeeTable[indexMax]);
-////    Console.WriteLine();
-
-
-////}
-//string wiersz1 = "FFF";
-
-//Console.WriteLine(emploeeTable[1]);
-//Console.WriteLine(wiersz1);
-
-////Console.WriteLine(EmploeeTable.ElementAt(1));
-
-
-//Console.WriteLine(max);
-//Console.WriteLine(indexMax);
-//Console.WriteLine(min);
-//Console.WriteLine(indexMin);
-
-
-
-
-
-////for (int i = 0; i <= WorkList1.Count; i++)
-
-
-
-
-
-//foreach (var ddd in TemporaryList)
-//{
-//    Console.WriteLine(ddd);
+//    Console.WriteLine(emploeeWinner.Name + " " + emploeeWinner.Surname + " lat " + emploeeWinner.Years);
 //}
