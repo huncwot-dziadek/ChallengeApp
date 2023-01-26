e2634200a80b4585c9ee0a0027a29af89e142216
huncwot-dziadek <huncwot_prawdziwy@wp.pl>
2023-01-26 11:40:10 +0100

    26 stycznia godz 11.30

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -5,7 +5,7 @@ Emploee emploee1 = new Emploee("Maciej", "Kramarczyk", "24");
 Emploee emploee2 = new Emploee("Anna", "Nienowacka", "33");
 Emploee emploee3 = new Emploee("Waldemar", "Pasikonik", "40");
 
-emploee1.AddScore(5);
+emploee1.AddScore(1);
 emploee1.AddScore(5);
 emploee1.AddScore(5);
 emploee1.AddScore(5);
@@ -13,14 +13,14 @@ emploee1.AddScore(5);
 
 emploee2.AddScore(5);
 emploee2.AddScore(5);
-emploee2.AddScore(5);
-emploee2.AddScore(5);
+emploee2.AddScore(7);
+emploee2.AddScore(7);
 emploee2.AddScore(5);
 
 emploee3.AddScore(5);
-emploee3.AddScore(5);
-emploee3.AddScore(5);
-emploee3.AddScore(5);
+emploee3.AddScore(9);
+emploee3.AddScore(7);
+emploee3.AddScore(3);
 emploee3.AddScore(5);
 
 Console.WriteLine(emploee1.Result);
@@ -34,25 +34,18 @@ List<Emploee> emploeeTable = new List<Emploee>()
 
 if (emploee1.Result == emploee2.Result && emploee1.Result == emploee3.Result)
 {
-    for (int j = 0; j < 1; j++)
-    {
-        Console.WriteLine($"Wszyscy pracownicy uzyskali tę samą liczbę punktów, tj. {emploee1.Result}");
-        Console.WriteLine("Są to:");
-        Console.WriteLine();
-        Console.WriteLine(emploee1.Name + " " + emploee1.Surname + " lat " + emploee1.Years);
-        Console.WriteLine(emploee2.Name + " " + emploee2.Surname + " lat " + emploee2.Years);
-        Console.WriteLine(emploee3.Name + " " + emploee3.Surname + " lat " + emploee3.Years);
-    }
+    Console.WriteLine($"Wszyscy pracownicy uzyskali tę samą liczbę punktów, tj. {emploee1.Result}");
+    Console.WriteLine("Są to:");
+    Console.WriteLine();
+    Console.WriteLine(emploee1.Name + " " + emploee1.Surname + " lat " + emploee1.Years);
+    Console.WriteLine(emploee2.Name + " " + emploee2.Surname + " lat " + emploee2.Years);
+    Console.WriteLine(emploee3.Name + " " + emploee3.Surname + " lat " + emploee3.Years);
 }
 
-//else
-//{//
-    //    List<Emploee> nextWinnerTable = new List<Emploee>() { emploee1 };
-
-    
+else if (emploee1.Result != emploee2.Result && emploee1.Result != emploee3.Result && emploee2.Result != emploee3.Result)
+{
     int maxResult = -1;
     Emploee emploeeWinner = null;
-    Emploee nextWinner = null;
 
     foreach (var emploee in emploeeTable)
     {
@@ -64,22 +57,59 @@ if (emploee1.Result == emploee2.Result && emploee1.Result == emploee3.Result)
 
         else if (emploee.Result == maxResult)
         {
-            nextWinner = emploeeWinner;
             emploeeWinner = emploee;
         }
     }
-//}
+    Console.WriteLine($"Pracownikiem, który uzyskał największą liczbę punktów, tj. {maxResult} jest:");
+    Console.WriteLine("");
+    Console.WriteLine(emploeeWinner.Name + " " + emploeeWinner.Surname + " lat " + emploeeWinner.Years);
+}
 
-Console.WriteLine($"Lista pracowników, którzy uzyskali największą liczbę punktów, tj. {maxResult}");
-Console.WriteLine("jest to następująca:");
-Console.WriteLine();
-Console.WriteLine(emploeeWinner.Name + " " + emploeeWinner.Surname + " lat " + emploeeWinner.Years);
-Console.WriteLine(nextWinner.Name + " " + nextWinner.Surname + " lat " + nextWinner.Years);
+else
+{
+    if (emploee1.Result == emploee2.Result && emploee1.Result < emploee3.Result)
+    {
+        Console.WriteLine($"Pracownikiem, który uzyskał największą liczbę punktów, tj. {emploee3.Result} jest:");
+        Console.WriteLine("");
+        Console.WriteLine(emploee3.Name + " " + emploee3.Surname + " lat " + emploee3.Years);
+    }
+    else if (emploee1.Result == emploee2.Result && emploee1.Result > emploee3.Result)
+    {
+        Console.WriteLine($"Pracownikami, którzy uzyskali największą liczbę punktów, tj. {emploee1.Result} są:");
+        Console.WriteLine("");
+        Console.WriteLine(emploee1.Name + " " + emploee1.Surname + " lat " + emploee1.Years);
+        Console.WriteLine(emploee2.Name + " " + emploee2.Surname + " lat " + emploee2.Years);
+    }
+    else if (emploee2.Result == emploee3.Result && emploee1.Result > emploee2.Result)
+    {
+        Console.WriteLine($"Pracownikiem, który uzyskał największą liczbę punktów, tj. {emploee1.Result} jest:");
+        Console.WriteLine("");
+        Console.WriteLine(emploee1.Name + " " + emploee1.Surname + " lat " + emploee1.Years);
+    }
+    else if (emploee2.Result == emploee3.Result && emploee1.Result < emploee2.Result)
+    {
+        Console.WriteLine($"Pracownikami, którzy uzyskali największą liczbę punktów, tj. {emploee2.Result} są:");
+        Console.WriteLine("");
+        Console.WriteLine(emploee2.Name + " " + emploee2.Surname + " lat " + emploee2.Years);
+        Console.WriteLine(emploee3.Name + " " + emploee3.Surname + " lat " + emploee3.Years);
+    }
+
+    else if (emploee1.Result == emploee3.Result && emploee1.Result < emploee2.Result)
+    {
+        Console.WriteLine($"Pracownikiem, który uzyskał największą liczbę punktów, tj. {emploee2.Result} jest:");
+        Console.WriteLine("");
+        Console.WriteLine(emploee2.Name + " " + emploee2.Surname + " lat " + emploee2.Years);
+    }
+
+    else
+    {
+        Console.WriteLine($"Pracownikami, którzy uzyskali największą liczbę punktów, tj. {emploee1.Result} są:");
+        Console.WriteLine("");
+        Console.WriteLine(emploee1.Name + " " + emploee1.Surname + " lat " + emploee1.Years);
+        Console.WriteLine(emploee3.Name + " " + emploee3.Surname + " lat " + emploee3.Years);
+    }
 
 
+}
 
 
-//foreach (var emploee in winnersTable)
-//{
-//    Console.WriteLine(emploeeWinner.Name + " " + emploeeWinner.Surname + " lat " + emploeeWinner.Years);
-//}
